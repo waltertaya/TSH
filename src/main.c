09e919892c98d930cdac8a171c265aa7160b5c6c
waltertaya <waltermittybecker5@gmail.com>
2026-05-11 16:43:21 +0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -5,8 +5,7 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
-  // printf("$ ");
+  printf("$ ");
 
   return 0;
 }
