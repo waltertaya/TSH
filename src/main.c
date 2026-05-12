0a9910229d807408924dbabbc71702799bbec4f1
waltertaya <waltermittybecker5@gmail.com>
2026-05-12 14:20:36 +0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -7,11 +7,13 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  printf("$ ");
-  char command[MAXLINE];
-  fgets(command, sizeof(command), stdin);
-  command[strcspn(command, "\n")] = '\0';
-  printf("%s: command not found\n", command);
+  while (1) {
+    printf("$ ");
+    char command[MAXLINE];
+    fgets(command, sizeof(command), stdin);
+    command[strcspn(command, "\n")] = '\0';
+    printf("%s: command not found\n", command);
+  }
 
   return 0;
 }
