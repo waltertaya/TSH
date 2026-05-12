eca725d6fec6a0029bd8e5ebea81572a9eb0d832
waltertaya <waltermittybecker5@gmail.com>
2026-05-12 15:00:39 +0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -17,7 +17,12 @@ int main(int argc, char *argv[]) {
     if (strcmp(command, "exit") == 0)
       break;
     
-    printf("%s: command not found\n", command);
+    // echo command
+    if (strncmp(command, "echo ", 5) == 0) {
+      printf("%s\n", command+5);
+    } else {
+      printf("%s: command not found\n", command);
+    }
   }
 
   return 0;
