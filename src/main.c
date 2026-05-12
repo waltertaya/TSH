ed56b2a6cc7c396936c5005ee8540ac7ab685e5f
waltertaya <waltermittybecker5@gmail.com>
2026-05-12 13:47:11 +0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define MAXLINE 1024
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
   printf("$ ");
+  char command[MAXLINE];
+  fgets(command, sizeof(command), stdin);
+  command[strcspn(command, "\n")] = '\0';
+  printf("%s: command not found\n", command);
 
   return 0;
 }
